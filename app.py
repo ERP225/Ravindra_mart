134bfbae6e96755f95645b73b88718dd5af4a380
Ravindra <ravindrakesamreddy123@gmail.com>
2026-04-13 15:10:02 +0530

    get otp

--- app.py
+++ app.py
@@ -36,8 +36,8 @@ app.config['UPLOAD_FOLDER'] = PROFILE_UPLOAD_FOLDER
 app.config.update(
     MAIL_SERVER='smtp.gmail.com',
     MAIL_PORT=465,
-    MAIL_USE_TLS=True,
-    MAIL_USE_SSL=False,
+    MAIL_USE_TLS=False,
+    MAIL_USE_SSL=True,
     MAIL_USERNAME=os.environ.get("MAIL_USERNAME"),
     MAIL_PASSWORD=os.environ.get("MAIL_PASSWORD"),
     MAIL_DEFAULT_SENDER=os.environ.get("MAIL_USERNAME"),
